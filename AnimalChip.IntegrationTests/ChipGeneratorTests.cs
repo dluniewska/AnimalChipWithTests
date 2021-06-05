f0c9d639430f425c6f594821dc6c9869135f748d
dluniewska <luniewska.d@gmail.com>
2021-06-05 21:28:56 +0200

    AnimalChip project with Tests

---
+++ AnimalChip.IntegrationTests/ChipGeneratorTests.cs
@@ -0,0 +1,61 @@
+﻿using AnimalChip.ModelCRUDwithoutDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AnimalChip.IntegrationTests
+{
+    public class ChipGeneratorTests
+    {
+        [Fact]
+        public void GenerateNumericChip_GeneratingSuccess_ReturnsInt()
+        {
+            var chip = ChipGenerator.generateNumericChip();
+
+            Assert.NotEmpty(ChipGenerator.chips);
+            Assert.InRange(chip, 100000, 100000000);
+        }
+
+        [Fact]
+        public void GenerateTextChip_GeneratingLowerCase_ReturnsStringToLower()
+        {
+            var chip = ChipGenerator.generateTextChipChooseCase(true);
+
+            Assert.NotEmpty(ChipGenerator.chips);
+            Assert.InRange(chip.Length, 6, 8);
+            Assert.Equal(chip.ToLower(), chip);
+        }
+
+        [Fact]
+        public void GenerateTextChip_GeneratingUpperCase_ReturnsStringBigcase()
+        {
+            var chip = ChipGenerator.generateTextChipChooseCase(false);
+
+            Assert.NotEmpty(ChipGenerator.chips);
+            Assert.InRange(chip.Length, 6, 8);
+            Assert.Equal(chip.ToUpper(), chip);
+        }
+
+        [Fact]
+        public void GenerateMixedChip_GeneratingNumbersWithLowerCase_ReturnsCharsAndNumbersLowercase()
+        {
+            var chip = ChipGenerator.generateMixedCharsAndNumbers(true);
+
+            Assert.NotEmpty(ChipGenerator.chips);
+            Assert.InRange(chip.Length, 6, 8);
+            Assert.StartsWith(chip.Substring(0, 6).ToLower(), chip);
+        }
+
+        [Fact]
+        public void GenerateMixedChip_GeneratingNumbersWithUpperCase_ReturnsCharsAndNumbersUppercase()
+        {
+            var chip = ChipGenerator.generateMixedCharsAndNumbers(false);
+
+            Assert.NotEmpty(ChipGenerator.chips);
+            Assert.InRange(chip.Length, 6, 8);
+            Assert.StartsWith(chip.Substring(0, 6).ToUpper(), chip);
+        }
+    }
+}
---
+++ AnimalChip.IntegrationTests/Helpers/CustomWebApplicationFactory.cs
@@ -0,0 +1,76 @@
+﻿using AnimalChip.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace AnimalChip.IntegrationTests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<ApplicationDbContext>));
+
+                services.Remove(descriptor);
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryTestDB");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        Utilities.InitializeDbForTests(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the " +
+                            "database with test messages. Error: {Message}", ex.Message);
+                    }
+                }
+            });
+        }
+    }
+
+    class FakeUserFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            context.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "123"),
+                new Claim(ClaimTypes.Name, "Test user"),
+                new Claim(ClaimTypes.Email, "test@example.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            }));
+
+            await next();
+        }
+    }
+
+
+}
